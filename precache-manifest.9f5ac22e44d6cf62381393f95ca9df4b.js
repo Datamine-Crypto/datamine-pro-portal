d42ac416cc2529c503458d827ea029ef6ac515da
Hodlforjesus <50343356+hodlforjesus@users.noreply.github.com>
2020-08-08 15:06:11 -0400

    Alpha Build 1

---
+++ precache-manifest.9f5ac22e44d6cf62381393f95ca9df4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf6085ce47313a358a9e17d2ac0f1b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "15177bb1fd9def21bda3",
+    "url": "./static/css/main.78820bce.chunk.css"
+  },
+  {
+    "revision": "94b9e6cc93c28f4a39a7",
+    "url": "./static/js/2.142fbcd2.chunk.js"
+  },
+  {
+    "revision": "15177bb1fd9def21bda3",
+    "url": "./static/js/main.1ae877e7.chunk.js"
+  },
+  {
+    "revision": "438cb369abb6f8164887",
+    "url": "./static/js/runtime-main.fc56244e.js"
+  },
+  {
+    "revision": "751bcc7f35923d29b9d9c7b877bf1c6e",
+    "url": "./static/media/logo.751bcc7f.svg"
+  }
+]);
