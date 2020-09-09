1dc9bda814ba9b3c82b3138bc81843ae7efb5f48
Hodlforjesus <50343356+hodlforjesus@users.noreply.github.com>
2020-09-08 21:13:35 -0400

    Transactions tab

--- precache-manifest.f4322da267acb0abe261890c8101fa77.js
+++ precache-manifest.f4322da267acb0abe261890c8101fa77.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "10b5bcd36455847cbf25c4d3b522c691",
+    "revision": "00c31fee43a3c0aecedf6ab4400f41fb",
     "url": "./index.html"
   },
   {
-    "revision": "1a3efc19d6517c72516b",
+    "revision": "cc465e13bcbc0eb787c8",
     "url": "./static/css/main.78820bce.chunk.css"
   },
   {
-    "revision": "c4e75bc4a9f95ed0cc5c",
-    "url": "./static/js/2.aaa9657a.chunk.js"
+    "revision": "cbb7ca2ca273770f65c9",
+    "url": "./static/js/2.564b2b47.chunk.js"
   },
   {
-    "revision": "1a3efc19d6517c72516b",
-    "url": "./static/js/main.d353f36b.chunk.js"
+    "revision": "cc465e13bcbc0eb787c8",
+    "url": "./static/js/main.f64fc629.chunk.js"
   },
   {
     "revision": "438cb369abb6f8164887",
@@ -22,5 +22,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "751bcc7f35923d29b9d9c7b877bf1c6e",
     "url": "./static/media/logo.751bcc7f.svg"
+  },
+  {
+    "revision": "a7241cd9533011f34ccca84901c2cb4b",
+    "url": "./static/media/uniswap.a7241cd9.svg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bc00dbbe9eb91949077639acaaac067a.js"
+  "./precache-manifest.f4322da267acb0abe261890c8101fa77.js"
 );
 
 self.addEventListener('message', (event) => {
